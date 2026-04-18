7a8a5f174e122853b9ea2e2c9a0602568d3b6bbc
Mohamed Ali <eng.mohamed.ali.alhamaimy@gmail.com>
2026-04-19 00:59:23 +0200

    Create Aggregation Engine

---
+++ AggregationEngine/src/main/java/com/mycompany/aggregationengine/AggregationEngine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aggregationengine;
+
+/**
+ *
+ * @author mohamed
+ */
+public class AggregationEngine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
